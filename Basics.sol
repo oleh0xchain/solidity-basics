76f3d2ab083c3bac0f2051fb9186d92939f188e5
oleh0xchain <oleh0xchain@gmail.com>
2025-04-04 13:44:39 +0200

    update: played with numbers and math

--- Basics.sol
+++ Basics.sol
@@ -2,19 +2,23 @@
 pragma solidity ^0.8.28;
 
 contract Basics {
-    string public greetings = "Hello World!"; 
-    string public name = "Oleh"; 
-    string public surname = "Bielous"; 
 
-    function getFullName() public view returns (string memory){
-        return string(abi.encodePacked(name, " ", surname));
-    }
+   uint public counter; 
 
-    function setGreetings (string calldata message) external {
-        greetings = message;
-    }
+   function setCounter (uint256 _count) public{
+      counter = _count;
+   } 
+   
+   function increment() public {
+      counter += 1; 
+   }
 
-    function getGreetings () public view returns (string memory){
-        return greetings;
-    }
+   function decrement () public{
+      counter-=1; 
+   }
+
+   function math () public{
+      counter = counter / 2; 
+   }
+
 }
