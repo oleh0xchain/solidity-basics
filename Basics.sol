e48490d6b1f6cd8a199e8c9a3b4432d3e855a406
oleh0xchain <oleh0xchain@gmail.com>
2025-04-05 18:29:37 +0200

    update: struct

--- Basics.sol
+++ Basics.sol
@@ -2,20 +2,54 @@
 pragma solidity ^0.8.28;
 
 contract Basics {
-   
-   mapping (address => uint256 ) public userBalances; 
 
-   function deposit() public payable {
-      userBalances[msg.sender] += msg.value; 
+  
+
+   struct UserData {
+      string name; 
+      uint256 age; 
+      bool isGay; 
+
+   }
+   mapping(address => UserData) public users;
+
+   UserData public me; 
+
+   function getUserName(address _userAddress) public view returns (string memory){
+      return users[_userAddress].name; 
    }
 
-   function addBalance(address _user, uint256 _balance) public{
-      userBalances [_user] = _balance;
-   } 
-
-   function getBalance(address _user) public view returns (uint256){
-      return userBalances[_user];
+   function getUserAge(address _userAddress) public view returns (uint256){
+      return users[_userAddress].age; 
    }
+
+   function getUserStatus(address _userAddress) public view returns (bool){
+      return users[_userAddress].isGay; 
+   }
+
+   function setUserData (string memory _name, uint256 _age, bool _isGay) public {
+      users[msg.sender] = UserData(_name, _age, _isGay); 
+   }
+
+
+   function setUserData1() public{
+      me = UserData("Nick", 26, false);
+   }
+
+   function setUserData2() public {
+      me = UserData({
+         name: "Mike",
+         age: 25,
+         isGay: true
+      });
+   }
+
+   function setUserData3() public{
+      me.name = "Ivan"; 
+      me.age = 54; 
+      me.isGay = false; 
+   }
+
 
 
 
